e1ced141f6d0656e318576478868db161f15cc4a
Lauren Jarrett <jarrett.lauren@gmail.com>
2019-07-28 12:37:22 +1000

    Updated src

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.MONGODB_URL_APP, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
