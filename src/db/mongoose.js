561c40fa1d495508e25c9d2bff470bdc0bfba1d2
Lauren Jarrett <jarrett.lauren@gmail.com>
2019-07-28 22:04:28 +1000

    testing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+console.log(process.env.MONGO_DB);
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
