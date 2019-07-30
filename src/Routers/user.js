856f8eb12d5784f7363b39430c56a4be86402233
Lauren Jarrett <jarrett.lauren@gmail.com>
2019-07-30 22:01:58 +1000

    Running on Prod

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -19,7 +19,6 @@ router.post('/users', async (req, res) => {
         
         res.status(201).send({ user, token })
     } catch (e) {
-        console.log(e);
         res.status(400).send(e)
     }
 })
